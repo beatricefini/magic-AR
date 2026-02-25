2a66c129c03fd5f43092e060161a0caf21c17aa6
beatricefini <finibeatrice@gmail.com>
2026-02-25 16:20:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   const marker = document.querySelector("#imageMarker");
   const model = document.querySelector("#magicModel");
+  const video = document.querySelector("#video1");
 
   marker.addEventListener("markerFound", () => {
     console.log("Marker trovato");
-    model.setAttribute("visible", "true");
+    model.setAttribute("visible","true");
+
+    // accendere il video
+    video.play();
+
+    // applicare il video al materiale "vid1" del modello
+    model.getObject3D('mesh').traverse((node) => {
+      if (node.isMesh && node.material.name === "vid1") {
+        node.material.map = new THREE.VideoTexture(video);
+        node.material.needsUpdate = true;
+      }
+    });
   });
 
   marker.addEventListener("markerLost", () => {
     console.log("Marker perso");
-    model.setAttribute("visible", "false");
+    model.setAttribute("visible","false");
+    video.pause();
   });
 });
