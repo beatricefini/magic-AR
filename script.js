348199a3cc57dea1080a579af6519c755cd3a2cb
beatricefini <finibeatrice@gmail.com>
2026-02-25 15:16:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const marker = document.querySelector("#imageMarker");
-  let cube = null;
+  const model = document.querySelector("#magicModel");
 
   marker.addEventListener("markerFound", () => {
     console.log("Marker trovato");
-
-    if (!cube) {
-      cube = document.createElement("a-box");
-
-      cube.setAttribute("color", "#00ffff");
-      cube.setAttribute("depth", "0.5");
-      cube.setAttribute("height", "0.5");
-      cube.setAttribute("width", "0.5");
-
-      cube.setAttribute("position", "0 0.25 0");
-
-      marker.appendChild(cube);
-    }
+    model.setAttribute("visible", "true");
   });
 
   marker.addEventListener("markerLost", () => {
     console.log("Marker perso");
-
-    if (cube) {
-      cube.remove();
-      cube = null;
-    }
+    model.setAttribute("visible", "false");
   });
 });
